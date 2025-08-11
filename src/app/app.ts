430b926ea14b413f4ad8e2ebf434979e5b95340d
Pilliveda <pillivedasatyasuma@gmail.com>
2025-08-11 21:58:52 +0530

    inital changes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,27 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Directive } from '@angular/core';
+import { LowerCasePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+   standalone: true,
+  imports: [RouterOutlet,CommonModule,LowerCasePipe,FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Directives');
+  names= ['PILLI','veda','satya','suma']
+  correct=true
+  wrong=false
+  data="s";
+  arr=['mi','readmi','apple','orange']
+  num1:number=3;
+  num2:number=4;
+
+  expression:string=""
+
 }
